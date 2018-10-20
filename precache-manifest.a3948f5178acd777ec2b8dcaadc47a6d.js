75bd96ead052baca40aef542418b38f82ee80335
byt3_h34d <mr.tybopp@gmail.com>
2018-10-20 08:35:28 -0500

    Updates

---
+++ precache-manifest.a3948f5178acd777ec2b8dcaadc47a6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86a184d192d777955c4aa008369c206e",
+    "url": "/ClickyGame/static/media/clouds2.86a184d1.jpg"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "a694de4d76bed0026cf3",
+    "url": "/ClickyGame/static/js/main.a694de4d.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/ClickyGame/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "a694de4d76bed0026cf3",
+    "url": "/ClickyGame/static/css/main.58231883.chunk.css"
+  },
+  {
+    "revision": "36c1ee1224384c947e69817baa014a2e",
+    "url": "/ClickyGame/index.html"
+  }
+];
